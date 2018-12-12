f0f997d503edd9bc71498f1f6f8a5fdf708da797
Niki Kokkinos <Niki@Nikis-MacBook-Air.local>
2018-12-12 14:18:33 -0500

    added styling for polygons

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,22 @@ L.tileLayer('https://a.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png
 	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
 
-var Sept2018Primary = L.geoJSON (Sept2018Primary, {
-  fillColor: "#c13432",
-  fillOpacity: .5,
-  weight: 1,
-  color: "#c13432",
-}).addTo(mymap);
+function totalvotecount(Queens_County_Governor_Democratic_Primary_Sept_2018_Total) {
+    return Queens_County_Governor_Democratic_Primary_Sept_2018_Total > 400  ? '#980043' :
+           Queens_County_Governor_Democratic_Primary_Sept_2018_Total > 300  ? '#dd1c77' :
+           Queens_County_Governor_Democratic_Primary_Sept_2018_Total > 200  ? '#df65b0' :
+           Queens_County_Governor_Democratic_Primary_Sept_2018_Total > 100  ? '#d7b5d8' :
+           Queens_County_Governor_Democratic_Primary_Sept_2018_Total > 0   ? '#f1eef6' :
+                      '#ffffff' ;}
+
+function style(feature) {
+    return {
+        fillColor: totalvotecount(feature.properties.Queens_County_Governor_Democratic_Primary_Sept_2018_Total),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };}
+
+var Sept2018Primary = L.geoJson(Sept2018Primary, {style: style}).addTo(mymap);
