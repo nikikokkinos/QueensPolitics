5ca62886061bb7c9a614030451e94b9b3dba0039
Niki Kokkinos <Niki@Nikis-MacBook-Air.local>
2018-12-14 14:00:20 -0500

    choropleth map colors established

--- js/scripts.js
+++ js/scripts.js
@@ -5,22 +5,25 @@ L.tileLayer('https://a.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png
 	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
 
-function totalvotecount(Queens_County_Governor_Democratic_Primary_Sept_2018_Total) {
-    return Queens_County_Governor_Democratic_Primary_Sept_2018_Total > 400  ? '#980043' :
-           Queens_County_Governor_Democratic_Primary_Sept_2018_Total > 300  ? '#dd1c77' :
-           Queens_County_Governor_Democratic_Primary_Sept_2018_Total > 200  ? '#df65b0' :
-           Queens_County_Governor_Democratic_Primary_Sept_2018_Total > 100  ? '#d7b5d8' :
-           Queens_County_Governor_Democratic_Primary_Sept_2018_Total > 0   ? '#f1eef6' :
-                      '#ffffff' ;}
+function totalvotecount(QueensCountyGovernorDemocraticPrimarySept2018_Total) {
+    return QueensCountyGovernorDemocraticPrimarySept2018_Total > 350  ? '#980043' :
+           QueensCountyGovernorDemocraticPrimarySept2018_Total > 300  ? '#dd1c77' :
+           QueensCountyGovernorDemocraticPrimarySept2018_Total > 200  ? '#df65b0' :
+           QueensCountyGovernorDemocraticPrimarySept2018_Total > 100  ? '#d7b5d8' :
+           QueensCountyGovernorDemocraticPrimarySept2018_Total > 50   ? '#f1eef6' :
+                                                                        '#ffffff'
+      ;}
 
 function style(feature) {
     return {
-        fillColor: totalvotecount(feature.properties.Queens_County_Governor_Democratic_Primary_Sept_2018_Total),
+        fillColor: totalvotecount(feature.properties.QueensCountyGovernorDemocraticPrimarySept2018_Total),
         weight: 2,
         opacity: 1,
-        color: 'white',
+        // color: 'white',
         dashArray: '3',
         fillOpacity: 0.7
-    };}
+      };}
 
-var Sept2018Primary = L.geoJson(Sept2018Primary, {style: style}).addTo(mymap);
+var QueensElectionDistricts = L.geoJson(QueensElectionDistricts, {style: style}).addTo(mymap);
+
+console.log(totalvotecount);
